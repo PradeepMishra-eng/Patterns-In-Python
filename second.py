bc8435e6be889dbb9ed6cb3dbbcde909463e4788
Ayush05m <72142971+Ayush05m@users.noreply.github.com>
2020-10-07 18:45:31 +0530

    Update second.py

--- second.py
+++ second.py
@@ -1,5 +1,5 @@
-lines = int(input('How many star line in Star Pattern # 6 ?:'))
-print('The required star pattern is:')
+lines = int(input('How many star line in Star Pattern you want # 6 ? : '))
+print('The desired star pattern is:')
 
 for i in range(lines, 0, -1):
     for k in range(lines, i, -1):
